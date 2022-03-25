a1514147921e54f68bc82c16a760337a2f26cf75
JacobC182 <84648161+JacobC182@users.noreply.github.com>
2022-03-25 15:16:55 +0000

    tidy

--- PartB.m
+++ PartB.m
@@ -1,78 +1,76 @@
-%Jacob Currie - 201718558 - ME527 Coursework
-%Part B: Solution - Surrogate Approach
-%---------------INITAL SETUP-----------------------------------------------
-tic; %start timer
-clear all %#ok<CLALL>     %clearing
-close all; clc;
-addpath('.\dace'); %Importing DACE library
-rng('default'); rng(12345); %make results repeatable but still stochastic
-colors = [255 0 0; 255 100 0; 255 170 0; 168 180 0; 60 255 0; 0 255 200; 0 130 255; 0 0 255; 160 0 255; 255 0 120];
-dots = ['.','*','+','x','s','d','^','v','p','h']; %Plotting
-%Bounds
-lb = zeros(1,6); %true lower bounds
-ub = [10, 50, 200, 1000, 5000, 20000]; %true upper bounds
-%Function Handle
-Fn = @(x) AuxModel(x.*ub);  %INPUT SCALED [0-1], OUTPUT NOT SCALED
-%Kriging Surrogate variables
-lb_K = lb + 1e-5; %lower bounds with "close to zero" tolerance for surrogate
-ub_K = ones(1,6); %upper bounds fixed for input scaling
-theta = [1, 1, 1, 1, 1, 1]; %Initial Theta per-variable values for kriging
-%---------------CREATING SURROGATE-----------------------------------------
-%number of training samples for initial surrogate
-n = 12;
-xTrain1 = lhsdesign(n, 6, 'iterations',5); xTrain2 = xTrain1; %Sampling using latin hypercube (initial surrogate)
-yTrain = zeros(n, 2);
+clear all
+close all
+clc
 
-for i = 1:n; yTrain(i,:) = Fn(xTrain1(i,:)); end %Getting solutions for LHS samples
-yTrain1 = yTrain(:,1); yTrain2 = yTrain(:,2);
-%Creating first surrogate
-[dmodel1, perf] = dacefit(xTrain1,yTrain1, @regpoly1, @corrgauss, theta, lb_K, ub_K);
-[dmodel2, perf] = dacefit(xTrain2,yTrain2, @regpoly1, @corrgauss, theta, lb_K, ub_K);
-%---------------OPTIMISING SURROGATE - GA----------------------------------
-FnS1 = @(x) predictor(x, dmodel1); %Surrogate function handle
-FnS2 = @(x) predictor(x, dmodel2); %Surrogate function handle
-p = 20; g = 200; r = floor((300 - n)/p); %Surrogate Optimisation options - p=population, g=generations, r=number of iterations for surrogate improvement
-optionsGA = optimoptions('gamultiobj', 'TolFun',1e-4, 'PopulationSize',p, 'MaxGenerations',g, 'Display','off');
+rng('default')
+rng(220)
 
-for i = 1:r %Iteratively improving surrogate
-    [x1, f1, eFlag, outInfo, popFinal1, popScore] = gamultiobj(FnS1, 6, [], [], [], [], lb_K, ub_K, optionsGA);
-    [x2, f2, eFlag, outInfo, popFinal2, popScore] = gamultiobj(FnS2, 6, [], [], [], [], lb_K, ub_K, optionsGA);%GA to find optimal front of surrogate
+ndim=6; ub = [10, 50, 200, 1000, 5000, 20000];
+TRUE_F=@(x) AuxModel(x .*ub);
 
-    popFinal1 = unique(popFinal1, "rows"); %Removing duplicates from pareto points - DACEFIT does not like duplicate points!!
-    popFinal2 = unique(popFinal2, "rows");
+theta = [10 10 10 10 10 10]; lob = ones(ndim, 1) .* 1e-4; upb = ones(ndim, 1);
 
-    xTrain1 = cat(1, xTrain1, popFinal1); 
-    xTrain2 = cat(1, xTrain2, popFinal2); 
+%% DOE
+nn=7;
+xKept=lhsdesign(nn,ndim,'criterion','maximin','iterations',30);
 
-    for j = 1:size(popFinal1, 1)%Adding new pareto points to list of surrogate training samples (line above also)
-        tt = Fn(popFinal1(j,:));
-        yTrain1 = cat(1, yTrain1, tt(1));
-    end
 
-    for j = 1:size(popFinal2, 1)%Adding new pareto points to list of surrogate training samples (line above also)
-        tt = Fn(popFinal2(j,:));
-        yTrain2 = cat(1, yTrain2, tt(2));
-    end
-
-    [dmodel1, perf] = dacefit(xTrain1,yTrain1, @regpoly1, @corrgauss, theta, lb_K, ub_K); %Creating new surrogate with updated training points
-    [dmodel2, perf] = dacefit(xTrain2,yTrain2, @regpoly1, @corrgauss, theta, lb_K, ub_K);
+%% Evaluation of the two objectives
+yKept1=[];
+yKept2=[];
+for i=1:nn
+    [f]=TRUE_F(xKept(i,:));
+    yKept1=[yKept1; f(1)];
+    yKept2=[yKept2; f(2)];
 end
-fEvalTotal = n + (r*p); %Calculating total function evaluations - does NOT take into account reduction due to line 35!!
-disp("Total Problem Function Evaluations: " + num2str(fEvalTotal));
-%---------------OPTIMISATION USING SURROGATE - GA--------------------------
-FnS_Final = @(x) [FnS1(x), FnS2(x)];
 
-maxEval = 6000; %Genetic algorithm options for final optimisation
-popsize = 200;
-maxgen = round(maxEval/popsize);
-ftol = 1e-5;
-optionsGA = optimoptions('gamultiobj', 'TolFun',ftol, 'PopulationSize',popsize, 'MaxGenerations',maxgen, 'Display','off');%, 'PlotFcn',@gaplotpareto);
 
-figure(1); xlabel('F-1');ylabel('F-2');title('10x Pareto Fronts'); hold on; %Creating final figure for pareto fronts
-%GA optimisation routine - repeating 10 times
-for i = 1:10
-    [x, f, eFlag, outInfo, popFinal, popScore] = gamultiobj(FnS_Final, 6, [], [], [], [], lb_K, ub_K, optionsGA); %Finding pareto front with GA
-    figure(1);
-    scatter(f(:,1),f(:,2),35,colors(i,:)./255, dots(i)); %Plotting front
+addpath('.\dace')
+
+[dmodel1, perf1] = dacefit(xKept,yKept1, @regpoly0, @corrgauss, theta, lob, upb);
+[dmodel2, perf2] = dacefit(xKept,yKept2, @regpoly0, @corrgauss, theta, lob, upb);
+dmodel10=dmodel1;
+dmodel20=dmodel2;
+
+%% Definition of the surrogate model that should be passed to optimiser
+fg_1=@(x) predictor(x,dmodel1);
+fg_2=@(x) predictor(x,dmodel2);
+FITNESSFCN=@(x) [fg_1(x);fg_2(x)];
+
+ng=0;
+ngmax=0;
+
+while size(yKept1,1)<600 && ng<200
+    %% Setting of the GA options
+    PopSize=5000;
+    X0=lhsdesign(PopSize,ndim,'criterion','maximin','iterations',30);
+    optionsGA = optimoptions('gamultiobj','PopulationSize',PopSize,'PlotFcn',@gaplotpareto,'InitialPopulationMatrix',X0, 'UseParallel',true);
+    naddMax=6;
+    naddZ=0;
+    ngmax=ngmax+5;
+    %% Surrogate based Optimisation loop
+    while naddZ<=2 && size(yKept1,1)<600
+        ng=ng+1;
+        optionsGA = optimoptions('gamultiobj','PopulationSize',PopSize,'PlotFcn',@gaplotpareto,'MaxGenerations',ngmax,'InitialPopulationMatrix',X0,'Display','none','UseParallel',true);
+        [X,FVAL,EXITFLAG,OUTPUT,POPULATION,SCORE] = gamultiobj(FITNESSFCN,ndim,[],[],[],[],zeros(1,ndim),ones(1,ndim),[],optionsGA);
+        X0=POPULATION;
+        update_database
+        
+        if nadd>0
+            [dmodel1, perf1] = dacefit(xKept,yKept1, @regpoly0, @corrgauss, theta, lob, upb);
+            [dmodel2, perf2] = dacefit(xKept,yKept2, @regpoly0, @corrgauss, theta, lob, upb);
+            
+            
+            fg_1=@(x) predictor(x,dmodel1);
+            fg_2=@(x) predictor(x,dmodel2);
+            FITNESSFCN=@(x) [fg_1(x);fg_2(x)];
+        end
+        if nadd==0
+            naddZ=naddZ+1;
+        else
+            naddZ=0;
+        end
+        disp([size(yKept1) nadd naddZ])
+    end    
+    
 end
-disp('PART B - SURROGATE OPTIMISATION ROUTINE FINISHED'); toc; %stop timer
